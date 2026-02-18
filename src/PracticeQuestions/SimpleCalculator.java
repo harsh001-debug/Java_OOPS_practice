aedd0136bfdc3101eb069366f60a1e348a1bbedb
harsh001-debug <harshkt200@gmail.com>
2026-02-19 00:15:18 +0530

    second basic question done

---
+++ src/PracticeQuestions/SimpleCalculator.java
@@ -0,0 +1,27 @@
+package PracticeQuestions;
+
+import java.util.Scanner;
+
+/*
+Question 2 -> Create a class Calculator with the following methods:
+ -calculateSum(int number1, int number2) to calculate the sum of two numbers.
+ -calculateDifference(int number1, int number2) to calculate the difference between two numbers.
+ */
+class Calculator{
+    void calculateSum(int number1,int number2){
+        System.out.println("Sum : " + (number1 + number2) );
+    }
+
+    void calculateDifference(int number1,int number2){
+        System.out.println("Difference : " + (number1 - number2) );
+    }
+}
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+//        Scanner scan = new Scanner(System.in);
+        Calculator c1 = new Calculator();
+        c1.calculateSum(4,5);
+        c1.calculateDifference(4,9);
+    }
+}
