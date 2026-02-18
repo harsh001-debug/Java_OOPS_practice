b8a060db17e93650cbf5ed7913a56e07accdcbdf
harsh001-debug <harshkt200@gmail.com>
2026-02-19 01:16:22 +0530

    Rectangle question done

---
+++ src/PracticeQuestions/Rectangle.java
@@ -0,0 +1,56 @@
+package PracticeQuestions;
+
+/* 6. Create a class Rectangle with attributes length and width,
+      each defaulting to 1. The class should include set and get
+      methods for both attributes, and a method to calculate the area of the rectangle.
+*/
+class Rectangle {
+    private double length;
+    private double width;
+
+    Rectangle(){
+        this.length = 1;
+        this.width = 1;
+    }
+
+    Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    void getLength(){
+        System.out.println("Length : "+this.length);
+    }
+    void getWidth(){
+        System.out.println("Width : "+this.width);
+    }
+
+    void areaOfRectangle(){
+        double area = this.length * this.width;
+        System.out.println("Area of Rectangle : " + area);
+    }
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle();
+        r1.getLength();
+        r1.getWidth();
+        r1.areaOfRectangle();
+
+
+        Rectangle r2 = new Rectangle(5,7);
+        r2.getLength();
+        r2.getWidth();
+        r2.areaOfRectangle();
+    }
+}
+
+/* ------------>>>>>>>>>>OUTPUT<<<<<<<<<<<<-----------------
+
+Length : 1.0
+Width : 1.0
+Area of Rectangle : 1.0
+Length : 5.0
+Width : 7.0
+Area of Rectangle : 35.0
+
+ */
